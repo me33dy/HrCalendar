abc1be6189d999935e3303deacd2a2a5dbbe64fa
Eric Wu <ericwu@Erics-MacBook-Pro.local>
2014-07-04 09:53:38 -0700

    fix set user before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user, except: [:new, :create]
+	before_action :set_user, only: [:edit, :update, :show]
 	before_action :correct_user, only: [:edit, :update, :show]
 	def index
 		@users = User.all
@@ -30,11 +31,9 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		set_user
 	end
 
 	def update
-		set_user
 		if @user.update_attributes(user_params)
 			redirect_to @user
 		else
