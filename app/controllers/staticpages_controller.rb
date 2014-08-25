a1272bcc3da8acc2b63a4106fdedcbfd9560325c
Eric Wu <ericwu@Erics-MacBook-Pro-2.local>
2014-08-25 15:11:33 -0700

    remove extra files

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,2 +1,12 @@
 class StaticpagesController < ApplicationController
+	before_action :return_user
+	def home
+	end
+
+	private
+	def return_user
+		if current_user
+			redirect_to user_employees_path(current_user)
+		end
+	end
 end
